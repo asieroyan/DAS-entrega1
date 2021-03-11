3debe6154b435840650df32e907f9ca9296e68f0
asier <asier.oyanguren@gmail.com>
2021-03-11 16:56:46 +0100

    FEAT: Intent de registro creado y el de inicio de sesión modificado

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView btnClickRegistrar = findViewById(R.id.btnClickRegistrar);
+        btnClickRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textEmail = findViewById(R.id.editTextEmail);
+                String email = (String) textEmail.getText();
+
+                TextView textContrasena = findViewById(R.id.editTextContraseña);
+                String contrasena = (String) textContrasena.getText();
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/practica1/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.practica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
