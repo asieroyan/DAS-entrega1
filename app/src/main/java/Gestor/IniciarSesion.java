515e96ce40c3a305381c5ec5bfd3807e7b0cace3
Asier <asier.oyanguren@gmail.com>
2021-04-19 20:40:50 +0200

    FEAT: Inicio de Sesion empezado

---
+++ app/src/main/java/Gestor/IniciarSesion.java
@@ -0,0 +1,62 @@
+package Gestor;
+
+
+import android.content.Context;
+
+        import androidx.annotation.NonNull;
+        import androidx.work.Data;
+        import androidx.work.Worker;
+        import androidx.work.WorkerParameters;
+
+        import java.io.BufferedInputStream;
+        import java.io.BufferedReader;
+        import java.io.InputStreamReader;
+        import java.io.PrintWriter;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
+
+public class IniciarSesion extends Worker {
+    public IniciarSesion(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        String direccion = "http://10.0.2.2/webservices_login.php";
+        HttpURLConnection urlConnection = null;
+        try {
+            URL destino = new URL(direccion);
+            urlConnection = (HttpURLConnection) destino.openConnection();
+            urlConnection.setConnectTimeout(5000);
+            urlConnection.setReadTimeout(5000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+            String email= getInputData().getString("email");
+            String parametros = "email="+email;
+            out.print(parametros);
+            out.close();
+            int statusCode = urlConnection.getResponseCode();
+            if (statusCode == 200) {
+                BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line, result = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                inputStream.close();
+                Data resultados = new Data.Builder()
+                        .putString("resultado",result)
+                        .build();
+                return Result.success(resultados);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
